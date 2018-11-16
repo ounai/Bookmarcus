43250856432520bdf986004ae342cb3e7fb494c0
Daniel Koch <daniel.koch@helsinki.fi>
2018-11-16 18:17:09 +0200

    Testing test

---
+++ src/main/java/main/Dummy.java
@@ -0,0 +1,8 @@
+//File for testing test --- delete later!
+package main;
+
+public class Dummy {
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
---
+++ src/test/java/testDummy.java
@@ -0,0 +1,12 @@
+import main.Dummy;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class testDummy {
+
+    @Test
+    public void testHelloWorld() {
+        Dummy d = new Dummy();
+        assertEquals(d.helloWorld(), "Hello World");
+    }
+}
