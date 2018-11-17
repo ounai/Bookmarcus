7991f434efccd0a7576948e25db78bc401d7aa8b
Daniel Koch <daniel.koch@helsinki.fi>
2018-11-17 13:18:50 +0200

    Added Bookmark class

---
+++ src/main/java/main/Bookmark.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author daniel
+ */
+public class Bookmark {
+    private String name;
+    private int id;
+    private String description;
+    
+    public Bookmark(String name) {
+        this.name = name;
+    }
+    
+    public Bookmark(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
